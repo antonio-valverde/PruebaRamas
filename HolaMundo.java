a290633d8ebb73f4afa0d57674cefef684ca029f
Antonio-Valverde <avallem860@iesalmudeyne.es>
2024-10-16 08:35:21 +0200

    Version inicial del fichero HolaMundo.java sin colores

---
+++ HolaMundo.java
@@ -0,0 +1,9 @@
+/**
+ * Muestra por pantalla la frase "Hola Mundo"
+ */
+public class HolaMundo {
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo");
+        System.out.println("Aqui estamos haciendo pruebas con Git");
+    }
+}
